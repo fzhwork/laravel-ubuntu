4897f6c67e0c1aece342f1525aa0b07e1e283532
fzhwork <1258630614@qq.com>
2017-05-22 09:15:28 +0800

    修改首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Hello Word
                 </div>
 
                 <div class="links">
